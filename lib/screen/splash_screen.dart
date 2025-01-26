606ba3fc6e6ce0b6b3705e82a591ffc58a728198
JongHyun070105 <bobi09361@gmail.com>
2025-01-26 23:49:58 +0900

    EcoStep

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -20,35 +20,12 @@ class _SplashScreenState extends State<SplashScreen> {
       );
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF082B2A),
       body: Center(
-        child: Stack(
-          children: [
-            Image.asset(
-              'assets/img/logo.png',
-              height: MediaQuery.of(context).size.height * 0.4,
-            ),
-            Positioned(
-              top: MediaQuery.of(context).size.height * 0.3,
-              left: 0,
-              right: 0,
-              child: Text(
-                'ECO STEP',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: MediaQuery.of(context).size.width * 0.1,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF75C97A),
-                ),
-              ),
-            ),
-          ],
-        ),
+        // 로고와 텍스트 제거
       ),
     );
   }
